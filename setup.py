56d9003926f85b28d0c8c0431ded7fd12ae81ca5
aFayolle <99188458+aFayolle@users.noreply.github.com>
2024-01-30 09:18:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='GameOfLife-Fayolle-CDOF2',
+ version='1.0',
+ author='aFayolle',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
